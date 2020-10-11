0b70faa3923997c03f880f9f42baf57e7e66aa54
mohamedowi <58092282+mohamedowi@users.noreply.github.com>
2020-10-11 03:31:05 +0200

    Add files via upload

---
+++ sources.mk
@@ -0,0 +1,41 @@
+#******************************************************************************
+# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+#
+# Redistribution, modification or use of this software in source or binary
+# forms is permitted as long as the files maintain this copyright. Users are
+# permitted to modify this and use it to learn about the field of embedded
+# software. Alex Fosdick and the University of Colorado are not liable for any
+# misuse of this material.
+#
+#*****************************************************************************
+
+
+# Source Paths
+ifeq ($(PLATFORM), MSP432)
+        SOURCES = ./src/course1.c \
+ 		  ./src/data.c \
+ 		  ./src/main.c \
+                  ./src/memory.c \
+ 		  ./src/stats.c \
+                  ./src/interrupts_msp432p401r_gcc.c \
+                  ./src/startup_msp432p401r_gcc.c \
+                  ./src/system_msp432p401r.c
+else
+        SOURCES = ./src/course1.c \
+                  ./src/data.c \
+                  ./src/main.c \
+                  ./src/memory.c \
+                  ./src/stats.c
+endif
+
+# Include Paths
+ifeq ($(PLATFORM), MSP432)
+        INCLUDES = -Iinclude/common \
+                   -Iinclude/CMSIS \
+                   -Iinclude/msp432
+else
+        INCLUDES = -Iinclude/common
+endif
+
+
+
